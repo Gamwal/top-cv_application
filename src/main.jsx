5f24f2ff31b94c10a8991e9adb1364de932b7a83
Gamaliel Adun <adungamaliel@gmail.com>
2024-12-02 12:46:10 +0100

    feat: style updates

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import Icon from "@mdi/react";
-import { mdiChevronDown, mdiChevronUp } from "@mdi/js";
 import "./index.css";
-import GeneralInfo from "./components/GeneralInfo";
-import EducationInfo from "./components/EducationInfo";
-import ExperienceInfo from "./components/Experience";
+import PersonalInfoSection from "./components/GeneralInfo";
+import EducationInfoSection from "./components/EducationInfo";
+import ExperienceInfoSection from "./components/Experience";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -14,43 +12,26 @@ createRoot(document.getElementById("root")).render(
       <div id="input-pane">
         <form action="">
           <div id="general-section">
-            <div className="section-head">
-              <h3>Personal Information</h3>
-              <button>
-                <Icon path={mdiChevronDown} size={1} />
-                <Icon path={mdiChevronUp} size={1} />
-              </button>
-            </div>
-            <GeneralInfo />
+            <PersonalInfoSection />
           </div>
 
           <div id="education-section">
-            <div className="section-head">
-              <h3>Education</h3>
-              <button>
-                <Icon path={mdiChevronDown} size={1} />
-              </button>
-            </div>
-            <EducationInfo />
+            <EducationInfoSection />
           </div>
 
           <div id="experience-section">
-            <div className="section-head">
-              <h3>Work Experience</h3>
-              <button>
-                <Icon path={mdiChevronDown} size={1} />
-              </button>
-            </div>
-            <ExperienceInfo />
+            <ExperienceInfoSection />
           </div>
 
           <div id="form-buttons">
-            <button type="submit">Edit</button>
+            <button type="button">Edit</button>
             <button type="submit">Submit</button>
           </div>
         </form>
       </div>
-      <div id="document-pane">Document</div>
+      <div>
+        <div id="document-pane"></div>
+      </div>
     </div>
   </StrictMode>
 );
